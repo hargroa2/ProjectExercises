1518b49adf4f737624dbeda0af42e8c3a77a8916
Amanda Hargrove <amandahargrove98@gmail.com>
2022-10-13 18:23:09 -0400

    Adding javascript to calculator

--- calculator/app.js
+++ calculator/app.js
@@ -1,1 +1,74 @@
-console.log("This is for the calculator");
+// Grabbing the classes
+const keys = document.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+const calculator = document.querySelector(".calculator");
+
+keys.addEventListener("click", (event) => {
+  if (!event.target.closest("button")) return; //prevents value in grid gap of calculator being returned. Closest will return the button value nearest clicked
+
+  const key = event.target;
+  const keyValue = key.textContent;
+  const displayValue = display.textContent;
+  const { type } = key.dataset;
+  const { previousKeyType } = calculator.dataset;
+
+  //   Is this a number key?
+  if (type === "number") {
+    if (displayValue === "0" || previousKeyType === "operator") {
+      display.textContent = keyValue;
+    } else if (previousKeyType === "operator") {
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
+    }
+  }
+
+  //   Is this an operator key?
+  if (type === "operator") {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach((el) => (el.dataset.state = "")); //allows one operator button to be selected at a time
+    key.dataset.state = "selected";
+
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
+  }
+
+  if (type === "clear") {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach((el) => (el.dataset.state = ""));
+    key.dataset.state = "";
+
+    calculator.dataset.firstNumber = "0";
+    display.textContent = 0;
+  }
+
+  if (type === "equal") {
+    /*
+    Perform a calculation:
+    firstNumber + secondNumber
+    firstNumber - secondNumber
+    firstNumber * secondNumber
+    firstNumber / secondNumber
+    */
+    const firstNumber = parseInt(calculator.dataset.firstNumber);
+    const operator = calculator.dataset.operator;
+    const secondNumber = parseInt(displayValue);
+
+    if (operator)
+      display.textContent = calculate(firstNumber, operator, secondNumber);
+  }
+
+  calculator.dataset.previousKeyType = type;
+});
+
+const calculate = (firstNumber, operator, secondNumber) => {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+
+  let result = "";
+  if (operator === "plus") result = firstNumber + secondNumber;
+  if (operator === "minus") result = firstNumber - secondNumber;
+  if (operator === "times") result = firstNumber * secondNumber;
+  if (operator === "divide") result = firstNumber / secondNumber;
+  return result;
+};
--- calculator/breakdown.md
+++ calculator/breakdown.md
@@ -1,11 +1,16 @@
 # Calculator: HTML, CSS, JS
 
+Created by Zell on Kevin Powell's YouTube, this basic calculator allows you to do standard operations.
+
 This calculator has a setup using several structures and techniques:
 
 ## HTML & CSS
 
-- div container -> div calculator -> calculator display, calculator keys
-- Inside of the calculator keys div, there are 17 buttons which all have individual classes for grid area locations (grid-template-areas and grid-area: name) and then have either two classes, operator and number, for background colors.
-- The calculator display is just a box with fixed width and padding with the zero inside text-aligned to the right
+- HTML file structure: div container -> div calculator -> calculator display, calculator keys
+- Inside of the calculator keys div, there are 17 buttons which have:
+  - individual classes for numbers for their grid area locations (grid-template-areas and grid-area: name) and then have either two classes, operator and number, for background colors. These number and operator classes are turned into data-type for javascript reasons.
+  - The calculator display is just a box with fixed width and padding with the zero inside text-aligned to the right
 
 ## JS
+
+The JavaScript aspect of this particular calculator utilizes `dataset`, which allows you to easily set or get the values of custom '-data' prefixed HTML attributes on your elements. It works like a regular object and it gives you the power to do pretty interesting stuff with the components on the page. It returns a DOMStringMap object with the keys and values within.
