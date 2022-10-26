917cb7546829ce2574372a4a692f15a6d43c574f
Amanda Hargrove <amandahargrove98@gmail.com>
2022-10-26 17:11:58 -0400

    counter practice

--- reactmovieapp/movieapp/src/App.js
+++ reactmovieapp/movieapp/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <h1>hi</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
     </div>
   );
 };
