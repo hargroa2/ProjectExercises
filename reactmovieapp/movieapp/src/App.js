7f118e0576b9d3a34f52897fb93ea76d6e0e9485
Amanda Hargrove <amandahargrove98@gmail.com>
2022-10-27 00:17:58 -0400

    Adding dynamic movie cards

--- reactmovieapp/movieapp/src/App.js
+++ reactmovieapp/movieapp/src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=31738fd0";
 
+const movie1 = {
+  Title: "Amazing Spiderman Syndrome",
+  Year: "2012",
+  imdbID: "tt2586634",
+  Type: "movie",
+  Poster: "N/A",
+};
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -15,8 +27,30 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div class="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div class="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
